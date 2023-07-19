43d0190ec904edc7e138a26405039b40781d1bb3
Jesper Löfberg <jesper@egot.se>
2023-07-19 19:34:23 +0200

    Parameter controlled re-loading

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,8 +5,61 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-3.3.1.min
+//= require jquery-3.7.0.min
 //= require bootstrap
 //= require popper.min
 //= require tableFilter
 //= require_self
+$(document).ready(function () {
+    addClickListeners();
+});
+
+function addClickListeners() {
+    $('.momentCheck').on('click', function () {
+        //const moment = this.name;
+        //const selected = $('#' + moment).prop('checked');
+        //console.log('clicked on moment: ' + this.name + ' is selected: ' + selected.toString());
+        reLoadMe();
+    });
+    $('.klassCheck').on('click', function () {
+        const klass = this.name.toString();
+        const selected = $('#' + klass).prop('checked');
+        const moment = klass.split('_')[0]
+        if (selected) {
+            let momentSel = $('#' + moment);
+            if (!momentSel.prop('checked')) {
+                momentSel.prop('checked', true);
+            }
+        }
+        //console.log('clicked on klass: ' + this.name + ' is selected: ' + selected.toString());
+        reLoadMe();
+    });
+}
+
+function reLoadMe() {
+    const url = window.location.href.split('?')[0];
+
+    let urlParams = '';
+    urlParams = addParams(urlParams, 'tsm');
+    urlParams = addParams(urlParams, 'behallare');
+    urlParams = addParams(urlParams, 'inomhus');
+    urlParams = addParams(urlParams, 'utomhus');
+    urlParams = addParams(urlParams, 'fordon');
+
+    window.location.href = url + '?' + urlParams;
+}
+
+function addParams(params, moment) {
+    const momentIsChecked = $('#' + moment).prop('checked');
+    let momentParams = '';
+    if (momentIsChecked) {
+        momentParams += $('#' + moment + '_nw1').prop('checked') ? (momentParams.length > 0 ? ',1' : '1') : '';
+        momentParams += $('#' + moment + '_nw2').prop('checked') ? (momentParams.length > 0 ? ',2' : '2') : '';
+        momentParams += $('#' + moment + '_nw3').prop('checked') ? (momentParams.length > 0 ? ',3' : '3') : '';
+        if (momentParams.length === 0) {
+            momentParams = '1'
+        }
+        return (params.length > 0 ? params + '&' : '') + moment + '=' + momentParams;
+    }
+    return params;
+}
--- grails-app/views/layouts/_inputRow.gsp
+++ grails-app/views/layouts/_inputRow.gsp
@@ -1,17 +1,17 @@
 <div class="row">
     <div class="col-4">
         <div class="checkBox">
-            <g:checkBox checked="${checkMap[moment]}" name="${moment}"/>&nbsp;<g:message
+            <g:checkBox class="momentCheck" checked="${checkMap[moment]}" name="${moment}"/>&nbsp;<g:message
                     code="nose.work.class.${moment}.label" default="${moment}"/>
         </div>
     </div>
 
     <div class="col">
-        <g:checkBox checked="${checkMap[moment+'_nw1']}" name="${moment}_nw1"/>&nbsp;<g:message
+        <g:checkBox class="klassCheck" checked="${checkMap[moment+'_nw1']}" name="${moment}_nw1"/>&nbsp;<g:message
                 code="nose.work.moment.one.label" default="one"/>
-        <g:checkBox checked="${checkMap[moment+'_nw2']}" name="${moment}_nw2"/>&nbsp;<g:message
+        <g:checkBox class="klassCheck" checked="${checkMap[moment+'_nw2']}" name="${moment}_nw2"/>&nbsp;<g:message
                 code="nose.work.moment.two.label" default="two"/>
-        <g:checkBox checked="${checkMap[moment+'_nw3']}" name="${moment}_nw3"/>&nbsp;<g:message
+        <g:checkBox class="klassCheck" checked="${checkMap[moment+'_nw3']}" name="${moment}_nw3"/>&nbsp;<g:message
                 code="nose.work.moment.three.label" default="three"/>
     </div>
 </div>
