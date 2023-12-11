8693a33ce18b48004081eb9d8287f386c5c4c623
Jesper Löfberg <jesper@egot.se>
2023-12-11 08:18:43 +0100

    Adapt to new HTML layaout

--- grails-app/init/snwk/BootStrap.groovy
+++ grails-app/init/snwk/BootStrap.groovy
@@ -7,12 +7,12 @@ class BootStrap {
             log.info('Adding profiles')
             LocalProfile lp = new LocalProfile()
             lp.profileName = 'Anna'
-            lp.profileSettings = '{"tsm_nw3":false,"tsm_nw2":true,"behallare_nw2":true,"tsm_elit":true,"nw1_doftprov":true}'
+            lp.profileSettings = '{"tsm_nw3":true,"tsm_nw2":false,"behallare_nw2":true,"tsm_elit":true,"nw1_doftprov":true}'
             lp.save failOnError: true
 
             lp = new LocalProfile()
             lp.profileName = 'Jesper'
-            lp.profileSettings = '{"tsm_nw3":true,"behallare_nw1":true,"tsm_elit":true,"utomhus_nw1":true,"inomhus_nw2":true,"fordon_nw1":true}'
+            lp.profileSettings = '{"tsm_nw3":false,"behallare_nw1":true,"tsm_elit":true,"utomhus_nw1":true,"inomhus_nw2":true,"inomhus_nw3":true,"fordon_nw1":true,"nw3_doftprov":true}'
             lp.save failOnError: true
         }
     }
